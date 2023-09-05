83dbe2787fe6fefcee499bae76c21e4b4e970ea3
Ken nguyen <kennguyen@brickmate.kr>
2023-09-05 08:20:12 +0700

    Add test yarn test items.service

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -1,18 +1,40 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ItemsService } from './items.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {ItemsService} from './items.service';
+import {getRepositoryToken} from "@nestjs/typeorm";
+import {Item} from "../entities/item.entity";
+import {EntityManager, Repository} from "typeorm";
 
 describe('ItemsService', () => {
-  let service: ItemsService;
+    let service: ItemsService;
+    let itemRepo: Repository<Item>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ItemsService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                ItemsService,
+                {
+                    provide: getRepositoryToken(Item),
+                    useValue: {
+                        find: jest.fn()
+                    }
+                },
+                {
+                    provide: EntityManager,
+                    useValue: {}
+                }
+            ],
+        }).compile();
 
-    service = module.get<ItemsService>(ItemsService);
-  });
+        service = module.get<ItemsService>(ItemsService);
+        itemRepo = module.get<Repository<Item>>(getRepositoryToken(Item));
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    test('find all', async () => {
+        await service.findAll()
+        expect(itemRepo.find).toHaveBeenCalled()
+    })
 });
